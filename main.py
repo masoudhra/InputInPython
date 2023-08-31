ebbf7b7ff920f1f84643f51b680b3c9ec732f92f
Masoud Ramezani <masoudhra@gmail.com>
2023-08-31 18:50:23 +1200

    work with input in python

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("Please insert your name: ")
+team = input("please insert your favorite soccer team: ")
+print("Hello " + name + "!\nYour favorite team is :" + team )
+# input be soorate string ast age bekhaym number bede behemoon baya tabdilesh konim
